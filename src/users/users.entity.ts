c6bbe6bc940ce700415553ea31216560185dfe7f
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-14 18:48:42 +0200

    add functionality of creating a user

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, AfterInsert, AfterUpdate, AfterRemove } from "typeorm";
 
 @Entity()
 export class UserEntity{
@@ -8,5 +8,18 @@ export class UserEntity{
     email: string
     @Column()
     password: string
+
+    @AfterInsert()
+    logInsert(){
+        console.log('the user is inserted with id', this.id)
+    }
+    @AfterUpdate()
+    logUpdate(){
+        console.log('the user is updated with id', this.id)
+    }
+    @AfterRemove()
+    logDelete(){
+        console.log('the user is deleted with id', this.id)
+    }
 
 }
