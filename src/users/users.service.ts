a4e323aa9e8ea8d9c1d6d701384d0433d07b50c8
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-14 10:59:36 +0200

    create user service and access it in controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserEntity } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(UserEntity) private Repo:Repository<UserEntity>){}
+       create(email: string, password: string){
+        const user = this.Repo.create({email, password})
+        return this.Repo.save(user)
+       }
+}
