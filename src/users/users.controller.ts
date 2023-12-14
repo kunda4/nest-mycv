1e749eff87d85787300464dbcee169caf1ab5d84
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-14 21:24:15 +0200

    add findOne functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -9,4 +9,9 @@ export class UsersController {
     createUser(@Body() body:CreateUserDto){
         return this.userService.create(body.email, body.password)
     }
+    @Get('/:id')
+    findUser(@Param('id') id:string){
+        return this.userService.findOne(parseInt(id));
+    }
 }
+
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,4 +10,26 @@ export class UsersService {
         const user = this.Repo.create({email, password})
         return this.Repo.save(user)
        }
+       findOne(id:number){
+        return this.Repo.findOne({where:{id}});
+       }
+       find(email:string){
+        return this.Repo.find({where:{email}})
+       }
+      async update(id:number, attrs:Partial<UserEntity>){
+        let user= await this.findOne(id)
+        if(!user){
+            throw Error('The id does not found')
+        }
+        Object.assign(user, attrs)
+        return this.Repo.save(user)
+       }
+      async remove(id:number){
+        const user = await this.findOne(id)
+        if(!user){
+            throw Error(`the user with an id ${id} not found`)
+        }
+        return this.Repo.remove(user)
+       }
 }
+
