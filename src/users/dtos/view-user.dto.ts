708cf9165a069b574838a94367d2aacffa90d1b9
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-20 14:13:24 +0200

    create user dto

---
+++ src/users/dtos/view-user.dto.ts
@@ -0,0 +1,8 @@
+import { Expose } from "class-transformer";
+
+export class ViewUserDto{
+    @Expose()
+    id:number;
+    @Expose()
+    email:string
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Body, ClassSerializerInterceptor, Controller, Delete, Get, NotFoundExce
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 
 @Controller('auth')
 export class UsersController {
@@ -10,7 +11,7 @@ export class UsersController {
     createUser(@Body() body:CreateUserDto){
         return this.userService.create(body.email, body.password)
     }
-    @UseInterceptors(ClassSerializerInterceptor)
+    @UseInterceptors(SerializeInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id:string){
         const user = await this.userService.findOne(parseInt(id));
