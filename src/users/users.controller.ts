d12ced6a3efcff664954ed3f470fe399e4856d13
Agnes Iradukunda <kundaaggy@gmail.com>
2023-12-21 14:47:12 +0200

    add functionality of checking the signin user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query, Session } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -12,13 +12,22 @@ export class UsersController {
     constructor(
         private userService:UsersService,
         private authService:AuthServices){}
+
+    @Get('/whoami')
+    whoAmI(@Session() session:any){
+        return this.userService.findOne(session.userId)
+    }   
     @Post('/signup')
-    createUser(@Body() body:CreateUserDto){
-        return this.authService.Signup(body.email, body.password)
-    }
+    async createUser(@Body() body:CreateUserDto, @Session() session:any){
+        const user = await this.authService.Signup(body.email, body.password)
+        session.userId = user.id
+        return user
+    } 
     @Post('/signin')
-    SignIn(@Body() body:CreateUserDto){
-        return this.authService.Signin(body.email, body.password)
+    async SignIn(@Body() body:CreateUserDto, @Session() session:any){
+        const user = await this.authService.Signin(body.email, body.password)
+        session.userId = user.id
+        return user
     }
    
     @Get('/:id')
