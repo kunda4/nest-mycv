c2083e4e3fc7506c7a85926e916deb1290030559
Agnes Iradukunda <kundaaggy@gmail.com>
2024-01-10 23:13:08 +0200

    create auth services test

---
+++ src/users/auth.services.spec.ts
@@ -0,0 +1,30 @@
+import {Test} from '@nestjs/testing'
+import { AuthServices } from './auth.services'
+import { UsersService } from './users.service'
+import { UserEntity } from './users.entity'
+import exp from 'constants'
+
+let service:AuthServices 
+beforeEach(async ()=>{
+    // create a fake copy of the user services
+    const fakeUsersServices: Partial<UsersService> = {
+        find:() => Promise.resolve([]),
+        create:(email:string, password:string) =>
+        Promise.resolve({id:1, email, password} as UserEntity),
+    };
+
+    const module = await Test.createTestingModule({
+        providers:[
+            AuthServices,
+            {
+                provide: UsersService,
+                useValue: fakeUsersServices,
+            },
+        ],
+    }).compile();
+
+     service = module.get(AuthServices);
+})
+it('can create an instance of auth service', async () =>{
+    expect(service).toBeDefined();
+})
