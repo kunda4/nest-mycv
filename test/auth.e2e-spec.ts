8f965124efa4101e4c1012ba527aa32e04dcc847
Agnes Iradukunda <kundaaggy@gmail.com>
2024-01-20 23:00:29 +0200

    working on end to end testing

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,29 +1,32 @@
-// import { Test, TestingModule } from "@nestjs/testing";
-// import { INestApplication, Post } from "@nestjs/common";
-// import * as request from 'supertest';
-// import { AppModule } from "./../src/app.module";
-// import { setupApp } from "./../src/settup-app";
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from 'supertest';
+import { AppModule } from "./../src/app.module";
+import { setupApp } from "./../src/settup-app";
 
-// describe('AuthController (e2e)', () => {
-//     let app: INestApplication;
+describe('AuthController (e2e)', () => {
+    let app: INestApplication;
 
-//     beforeEach(async () => {
-//         const moduleFixture: TestingModule = await Test.createTestingModule({
-//             imports: [AppModule],
-//         }).compile()
+    beforeEach(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile()
 
-//         app = moduleFixture.createNestApplication();
-//         setupApp(app)
-//         await app.init();
-//     })
-//     it('should create new user', async () =>{
-//         let email:string ='kunda@gmail.com'
-//         const res = await request(app.getHttpServer())
-//             .post('/auth/signup')
-//             .send({ email, password: 'kunda123' })
-//             .expect(201);
-//         const { id, email: email_1 } = res.body;
-//         expect(id).toBeDefined();
-//         expect(email_1).toEqual(email_1);
-//     })
-// })
+        app = moduleFixture.createNestApplication();
+        setupApp(app)
+        await app.init();
+    })
+    it('should create new user', async () =>{
+        let email ='kunda@gmail.com'
+        return request(app.getHttpServer())
+            .post('/auth/signup')
+            .send({ email, password: 'kunda123' })
+            .expect(201)
+            .then((res) => {
+                const { id, email } = res.body;
+                expect(id).toBeDefined();
+                expect(email).toEqual(email);
+            })
+
+    })
+})
